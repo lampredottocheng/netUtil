8961508c191292c4da5a950a83c46b953768210f
qiruonan <qiruonan26@yahoo.co.jp>
2023-06-27 16:06:02 +0900

    F画面实装common

---
+++ src/common/FMsgBean.java
@@ -0,0 +1,13 @@
+package common;
+
+public class FMsgBean {
+	//sample;-Cf1mJ>SCMSG
+	public static String sampleMsg = "sampleMsg print";	
+	
+	//A;-Cf1mJ>SCMSG
+	public static String aMsg = "AMsg print";
+
+	//A;-Cf1mJ>SCMSG
+	public static String fMsg = "FMsg print";
+
+}
